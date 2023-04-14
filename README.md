754bb19d9e585e1880f27b4a5f2ac5ec1da90e9c
Ole Kristian Fjaerestad <fjaerestad.olekristian@gmail.com>
2023-04-14 15:32:14 +0200

    Add 'refresh cookie with iframe'.

--- README.md
+++ README.md
@@ -5,15 +5,34 @@ Reauthenticate user in a single page application using http only cookie, without
 - You have a Single Page Application.
 - You use SSO and HTTP Only Cookies for authentication.
   - SSO involves request redirects (e.g. mydomain.com -> mysso.com -> mydomain.com).
-- You want to reauthenticate a user without the user having to reload the entire page. 
+- You want to reauthenticate a user (i.e. refresh cookies) without the user having to reload the entire page. 
 
 This proof of concept aims to solve this problem in a pragmatic (frontend-only) way, 
-not involving major rewrites of backend/architecture.
+not involving major changes to backend/architecture.
 
-## Solution
+## Solution 1: Refresh cookies with iframe
+- Add a button to the UI.
+- When clicking the button, an invisible iframe is appended to the DOM.
+- The iframe opens the same domain as the current one. 
+- When the iframe has finished loading the page (i.e. SSO reauthentication is done and all required cookies are set),
+  the iframe is automatically removed from the DOM. 
+- The user now has fresh cookies, and can continue where he/she left off.
+
+## Solution 2: Refresh cookies in new tab
 - Add a button to the UI.
 - When clicking the button, a new browser tab opens. 
 - The new tab opens the same domain as the current one. 
 - When the new tab has finished loading the page (i.e. SSO reauthentication is done and all required cookies are set),
   the tab automatically closes. 
-- The user is now back in the original tab, with a fresh cookie, and can continue where he/she left off.
+- The user is now back in the original tab, with fresh cookies, and can continue where he/she left off.
+
+## Solution 3: Refresh cookies with fetch request
+- Add a button to the UI.
+- When clicking the button, a fetch request is made to the server. 
+- SSO reauthentication kicks in, and eventually completes.
+- The server receives the request.
+- The server response includes one or multiple `Set-Cookie` headers with the required cookie(s). 
+- The user is now back in the original tab, with fresh cookies, and can continue where he/she left off.
+
+> Note: This solution is untested and probably requires changes to backend/architecture.
+  It might not even work; step 3 most likely doesn't (it might depend on the SSO provider).
--- client.js
+++ client.js
@@ -1,18 +1,58 @@
-document.getElementById('refreshCookieWithFetch').addEventListener('click', () => {
-  fetch('/api/refresh-cookie', { credentials: 'same-origin' })
-    .then(async (response) => console.log(await response.text()));
+function refreshCookieWithIframe() {
+  return new Promise((resolve, reject) => {
+    const iframe = document.createElement('iframe');
+
+    // Query parameter is optional. The iframe can use it if necessary.
+    iframe.src = '/?refresh-cookie';
+    iframe.style.position = 'fixed';
+    iframe.style.opacity = '0';
+    iframe.style.width = '0';
+    iframe.style.height = '0';
+    iframe.style.border = 'none';
+    iframe.style.pointerEvents = 'none';
+    iframe.setAttribute('aria-hidden', 'true');
+    document.body.append(iframe);
+
+    // We can either take a best guess at how long reauthentication takes:
+    // setTimeout(() => iframe.remove(), 3000);
+
+    // Or we can let the iframe decide when reauthentication is done:
+    iframe.contentWindow.addEventListener('DOMContentLoaded', () => {
+      iframe.remove();
+      resolve();
+    });
+  });
+}
+
+function refreshCookieInNewTab() {
+  return new Promise((resolve, reject) => {
+    // Query parameter is optional. The new window can use it if necessary.
+    const newWindow = window.open('/?refresh-cookie');
+
+    // We can either take a best guess at how long reauthentication takes:
+    // setTimeout(() => newWindow.close(), 3000);
+
+    // Or we can let the new window decide when reauthentication is done:
+    newWindow.addEventListener('DOMContentLoaded', () => {
+      newWindow.close();
+      resolve();
+    });
+  });
+}
+
+document.getElementById('refreshCookieWithIframe').addEventListener('click', async () => {
+  await refreshCookieWithIframe();
+  console.log('Refreshed cookie with iframe');
 });
 
-document.getElementById('refreshCookieInNewWindow').addEventListener('click', () => {
-  const newWindow = window.open('/?refresh-cookie');
+document.getElementById('refreshCookieInNewTab').addEventListener('click', async () => {
+  await refreshCookieInNewTab();
+  console.log('Refreshed cookie in new tab');
+});
 
-  // We can either take a best guess at how long reauthentication takes:
-  // setTimeout(() => newWindow.close(), 3000);
-
-  // Or we can let the new window decide when reauthentication is done:
-  newWindow.addEventListener('DOMContentLoaded', () => {
-    newWindow.close();
-  });
+document.getElementById('refreshCookieWithFetch').addEventListener('click', () => {
+  fetch('/api/refresh-cookie', { credentials: 'same-origin' })
+    .then(async (response) => console.log('Refreshed cookie with fetch request'));
 });
 
 document.getElementById('checkCookieValidity').addEventListener('click', () => {
